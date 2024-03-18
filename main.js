d8a47d9fba782c7ad3baf9bf471c9af42d8d306e
Erlan-asd <Vipoffacc@gmail.com>
2024-03-18 18:33:54 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// 1)------------------------------------------------
+
+let userData = {
+    name: "John"};
+
+// userData.age = prompt('Введите возраст')
+
+// console.log(userData);
+
+// // 2)------------------------------------------------
+
+// userData.underage = confirm()
+
+// console.log(userData);
+
+// // 3)------------------------------------------------
+
+// userData.color =prompt('Введите цвет')
+// console.log(userData);
+
+// delete userData.color
+// console.log(userData);
+
+// 4)------------------------------------------------
+// 5)------------------------------------------------
+userData.address = {
+    country: 'Kgz',
+    city: 'Bishkek',
+    street: 'Chui'
+}
+
+console.log(userData);
+
